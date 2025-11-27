9b5b558197d56b1b9e20abad2d3fa380af6617c6
Ayush-478 <147281294+Ayush-478@users.noreply.github.com>
2025-11-27 14:22:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ import { Client } from 'pg'
 
 const client = new Client({
   connectionString: "postgresql://postgres:CherryIsDoomed1234@db.fktebijqvaivayslpubf.supabase.co:5432/postgres",
-  ssl : { rejectUnauthorized: false}
+  ssl : { rejectUnauthorized: false},
+  family : 4
 })
 
 export default async function databaseSetup(){
