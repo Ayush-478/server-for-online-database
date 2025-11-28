b42a9ec00aa23daec015cb56a07cab228f8d48bd
Ayush-478 <147281294+Ayush-478@users.noreply.github.com>
2025-11-28 13:56:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import dbObj from './services/databaseOperations.js'
 import errorHandler from './middleware/errorHandler.js'
 import dotenv from 'dotenv'
 const app = express();
-const port = 5000
+const PORT = process.env.PORT || 5000
 const host = '0:0:0:0'
 
 const corsOptions = {
@@ -41,6 +41,6 @@ app.get('/', (req,res,next)=>{
 
 app.use(errorHandler)
 
-app.listen(port,host, ()=>{console.log("Server running"
+app.listen(PORT,host, ()=>{console.log("Server running"
 )})
 
